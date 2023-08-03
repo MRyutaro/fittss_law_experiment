4d2e80a6a3f809e7ea45838965c367fbc73adfdb
MRyutaro <m2002r1028@icloud.com>
2023-08-03 19:22:05 +0900

    add: 文字列でpython側からarduinoを操作できるようになった

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,33 +1,19 @@
-// 定義
-#define RIGHT_LED_PIN 12
-#define LEFT_LED_PIN 13
+#define RIGHT_LED 13
+#define LEFT_LED 12
 
-void setup() {
-  Serial.begin(9600); // シリアル通信の初期化
-  pinMode(RIGHT_LED_PIN, OUTPUT);
-  pinMode(LEFT_LED_PIN, OUTPUT);
-  digitalWrite(RIGHT_LED_PIN, LOW);
-  digitalWrite(LEFT_LED_PIN, LOW);
+void setup()
+{
+  Serial.begin(9600);
+  pinMode(RIGHT_LED, OUTPUT);
 }
 
 void loop() {
-  int data = Serial.read();
-
-  // 右のLEDを制御するコマンド
-  if (data = "right_led_on") {
-    Serial.println("00");
-    digitalWrite(RIGHT_LED_PIN, HIGH); // 右のLEDをオンにする
-  }
-  if (data = "right_led_off") {
-    Serial.println("01");
-    digitalWrite(RIGHT_LED_PIN, LOW); // 右のLEDをオフにする
-  }
-
-  // 左のLEDを制御するコマンド
-  if (data = "left_led_on") {
-    digitalWrite(LEFT_LED_PIN, HIGH); // 左のLEDをオンにする
-  }
-  if (data = "left_led_off") {
-    digitalWrite(LEFT_LED_PIN, LOW); // 左のLEDをオフにする
+  // put your main code here, to run repeatedly:
+  String message = Serial.readStringUntil('\n');
+  message.trim();
+  if (message == "ON") {
+    digitalWrite(RIGHT_LED, HIGH);
+  } else if (message == "OFF") {
+    digitalWrite(RIGHT_LED, LOW);
   }
 }
--- main.py
+++ main.py
@@ -2,58 +2,17 @@ import serial
 import time
 
 
-def right_led_on(ser):
-    str = "right_led_on"
-    ser.write(str.encode("utf-8"))
-    print("right_led_on")
-    time.sleep(1)
-
-
-def right_led_off(ser):
-    str = "right_led_off"
-    ser.write(str.encode("utf-8"))
-    print("right_led_off")
-    time.sleep(1)
-
-
-def left_led_on(ser):
-    str = "left_led_on"
-    ser.write(str.encode("utf-8"))
-    print("left_led_on")
-    time.sleep(1)
-
-
-def left_led_off(ser):
-    str = "left_led_off"
-    ser.write(str.encode("utf-8"))
-    print("left_led_off")
-    time.sleep(1)
-
-
 def main():
     #  COMポートを開く
     print("Open Port")
     ser = serial.Serial("COM3", 9600)
-    print(ser)
+    time.sleep(2)
+
     while True:
-        try:
-            right_led_on(ser)
-            right_led_off(ser)
-            left_led_on(ser)
-            left_led_off(ser)
-
-            line = ser.readline()
-            line_disp = line.strip().decode('UTF-8')
-            print(line_disp)
-
-        except KeyboardInterrupt:
-            print("Close Port")
-            ser.close()
-
-        except Exception as e:
-            print(e)
-            print("Close Port")
-            ser.close()
+        ser.write('ON\n'.encode())
+        time.sleep(1.0)
+        ser.write('OFF\n'.encode())
+        time.sleep(1.0)
 
 
 if __name__ == '__main__':
