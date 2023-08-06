d3b3876925542e74a5937aabe797fd0319173ebf
MRyutaro <m2002r1028@icloud.com>
2023-08-06 16:05:17 +0900

    add: 入力と出力の同時制御

---
+++ arduino/main/main.ino
@@ -0,0 +1,73 @@
+#define RIGHT_LED 12
+#define LEFT_LED 13
+#define LEFT_VOL_PIN A0
+#define CENTER_VOL_PIN A1
+#define RIGHT_VOL_PIN A2
+
+int is_Right_LED_On = 0;
+int is_Left_LED_On = 0;
+float left_volt, center_volt, right_volt;
+
+void setup()
+{
+    Serial.begin(9600);
+    Serial.setTimeout(100);
+    pinMode(RIGHT_LED, OUTPUT);
+    pinMode(LEFT_LED, OUTPUT);
+}
+
+float getVoltage( int pin ){
+    int value;
+    float volt;
+
+    value = analogRead( pin );
+
+    volt = value * 5.0 / 1023.0;
+
+    return volt;
+}
+
+void loop()
+{   
+    // メッセージの受信
+    String message = Serial.readStringUntil('\n');
+    message.trim();
+
+    // LEDの制御
+    if (message == "right_led_on")
+    {
+        digitalWrite(RIGHT_LED, HIGH);
+        is_Right_LED_On = 1;
+    }
+    else if (message == "right_led_off")
+    {
+        digitalWrite(RIGHT_LED, LOW);
+        is_Right_LED_On = 0;
+    }
+    else if (message == "left_led_on")
+    {
+        digitalWrite(LEFT_LED, HIGH);
+        is_Left_LED_On = 1;
+    }
+    else if (message == "left_led_off")
+    {
+        digitalWrite(LEFT_LED, LOW);
+        is_Left_LED_On = 0;
+    };
+
+    // 電圧の取得
+    left_volt = getVoltage( LEFT_VOL_PIN );
+    center_volt = getVoltage( CENTER_VOL_PIN );
+    right_volt = getVoltage( RIGHT_VOL_PIN );
+
+    // メッセージの送信
+    Serial.print(is_Left_LED_On);
+    Serial.print(",");
+    Serial.print(is_Right_LED_On);
+    Serial.print(",");
+    Serial.print(center_volt);
+    Serial.print(",");
+    Serial.print(left_volt);
+    Serial.print(",");
+    Serial.println(right_volt);
+}
--- main.py
+++ main.py
@@ -8,10 +8,25 @@ import os
 
 # arduinoからのデータを受け取る関数
 def get_data_from_arduino(ser):
+    """
+    arduinoからのデータを受け取る関数
+
+    Parameters
+    ----------
+    ser : serial.Serial
+        シリアル通信の設定
+
+    Returns
+    -------
+    data_from_arduino : list
+        arduinoから受け取ったデータ. ['left_led', 'right_led', 'center_pin_volt', 'left_pin_volt', 'right_pin_volt']
+    """
     try:
         line = ser.readline()
         stripped_str = str(line, 'ascii').strip()
         data_from_arduino = [float(d) for d in stripped_str.split(',')]
+        now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+        print(f'{now}: {data_from_arduino}')
 
         return data_from_arduino
 
@@ -21,16 +36,35 @@ def get_data_from_arduino(ser):
 
 
 def insert_line(data_from_arduino, df):
+    """
+    データリストの先頭に現在の時間を追加して、DataFrameに新しい行を追加する関数
+
+    Parameters
+    ----------
+    data_from_arduino : list
+        arduinoから受け取ったデータ
+    df : DataFrame
+        全てのデータを保存するDataFrame
+    """
+    # data_from_arduinoのコピーを作成
+    insert_data = data_from_arduino.copy()
+
     # 現在の時間を取得
-    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+    current_time = datetime.now().strftime('%Y-%m-%d %H:%M:%S.%f')
 
     # データリストの先頭に現在の時間を追加
-    data_from_arduino.insert(0, current_time)
+    insert_data.insert(0, current_time)
 
     # DataFrameに新しい行を追加
-    df.loc[len(df)] = data_from_arduino
+    df.loc[len(df)] = insert_data
 
     return df
+
+
+# 全てのLEDを消す
+def all_led_off(ser):
+    ser.write('left_led_off\n'.encode())
+    ser.write('right_led_off\n'.encode())
 
 
 if __name__ == '__main__':
@@ -44,10 +78,6 @@ if __name__ == '__main__':
     ser = serial.Serial("COM3", 9600)
     time.sleep(2)
 
-    # 空のDataFrameを作成
-    columns = ['time', 'left_led', 'right_led', 'center_pin_volt', 'left_pin_volt', 'right_pin_volt']
-    saved_df = pd.DataFrame(columns=columns)
-
     # 真ん中の位置で安定させる
     count = 0
     while True:
@@ -56,10 +86,11 @@ if __name__ == '__main__':
             if data_from_arduino is None:
                 continue
 
-            saved_df = insert_line(data_from_arduino, saved_df)
+            if len(data_from_arduino) != 5:
+                continue
 
             # もしcenter_pin_voltが0.15V以下になるのが連続で10回あったらwhile文を抜ける
-            if data_from_arduino[3] < 0.15:
+            if data_from_arduino[2] < 1.25:
                 count += 1
                 if count == 10:
                     break
@@ -68,14 +99,20 @@ if __name__ == '__main__':
             print('ValueError')
             continue
 
+    print("実験を開始します")
+
+    # 空のDataFrameを作成
+    columns = ['time', 'left_led', 'right_led', 'center_pin_volt', 'left_pin_volt', 'right_pin_volt']
+    saved_df = pd.DataFrame(columns=columns)
+
     # ランダムな時間待つ
     sleep_time = random.randint(1, 10)
     time.sleep(sleep_time)
 
     # LEDを光らせる
     # ser.write('right_led_on\n'.encode())もしくはser.write('left_led_on\n'.encode())をランダムに選択
-    led = random.choice(['right_led_on', 'left_led_on'])
-    ser.write(f'{led}\n'.encode())
+    led = random.choice(['right_led_on\n', 'left_led_on\n'])
+    ser.write(led.encode())
     data_from_arduino = get_data_from_arduino(ser)
     saved_df = insert_line(data_from_arduino, saved_df)
 
@@ -90,6 +127,22 @@ if __name__ == '__main__':
 
             # 左右どっちかの電圧が0.15V以下になり、かつ、それがLEDが光っている方と同じだったらwhile文を抜ける
             # そうじゃなかったらexit()でプログラムを終了する
+            if data_from_arduino[3] < 1.25:
+                if led == 'left_led_on\n':
+                    print("実験が正常に終了しました")
+                    break
+                else:
+                    print("実験が異常終了しました。LEDは左側、選択したのは右側です")
+                    all_led_off(ser)
+                    exit()
+            elif data_from_arduino[4] < 1.25:
+                if led == 'right_led_on\n':
+                    print("実験が正常に終了しました")
+                    break
+                else:
+                    print("実験が異常終了しました。LEDは右側、選択したのは左側です")
+                    all_led_off(ser)
+                    exit()
 
         except ValueError:
             print('ValueError')
@@ -100,7 +153,10 @@ if __name__ == '__main__':
         os.makedirs(f'./data/{antilog}')
 
     # 現在の時間を取得
-    current_time = datetime.now().strftime('%Y%m%d%H%M%S')
+    current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
 
     # データをCSVファイルに保存
     saved_df.to_csv(f'./data/{antilog}/{current_time}.csv', index=False)
+
+    # LEDを消す
+    all_led_off(ser)
