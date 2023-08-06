6e763f5ba54ea8f789ef06ddac86f68cbd0e140e
MRyutaro <m2002r1028@icloud.com>
2023-08-06 18:12:47 +0900

    add: プログラム完成

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pandas as pd
 from datetime import datetime
 import random
 import os
+import math
 
 
 # arduinoからのデータを受け取る関数
@@ -71,9 +72,11 @@ if __name__ == '__main__':
     #####
     # ここを変える
     w = 0.5
-    d = 0.5
+    d = 30
     #####
     antilog = 2 * d / w
+    # idを計算. logを計算
+    id = math.log2(antilog)
 
     ser = serial.Serial("COM3", 9600)
     time.sleep(2)
@@ -89,7 +92,7 @@ if __name__ == '__main__':
             if len(data_from_arduino) != 5:
                 continue
 
-            # もしcenter_pin_voltが0.15V以下になるのが連続で10回あったらwhile文を抜ける
+            # もしcenter_pin_voltが1.25V以下になるのが連続で10回あったらwhile文を抜ける
             if data_from_arduino[2] < 1.25:
                 count += 1
                 if count == 10:
@@ -117,6 +120,9 @@ if __name__ == '__main__':
     saved_df = insert_line(data_from_arduino, saved_df)
 
     # 電圧を取得する
+    is_response = False
+    is_movement = False
+
     while True:
         try:
             data_from_arduino = get_data_from_arduino(ser)
@@ -125,11 +131,28 @@ if __name__ == '__main__':
 
             saved_df = insert_line(data_from_arduino, saved_df)
 
+            # data_from_arduinoの0か1番目の要素が1になったらそのときの時間を取得
+            if is_response is False:
+                if data_from_arduino[0] == 1.0 or data_from_arduino[1] == 1.0:
+                    is_response = True
+                    rt_start_time = datetime.now()
+
+            # data_from_arduinoの2番目の要素が1.25V以上になったらそのときの時間を取得
+            if is_movement is False:
+                if data_from_arduino[2] > 1.25:
+                    is_movement = True
+                    mt_start_time = datetime.now()
+                    print(f"mt_start_time: {mt_start_time}")
+                    print(f"rt_start_time: {rt_start_time}")
+                    rt_time = mt_start_time - rt_start_time
+
             # 左右どっちかの電圧が0.15V以下になり、かつ、それがLEDが光っている方と同じだったらwhile文を抜ける
             # そうじゃなかったらexit()でプログラムを終了する
             if data_from_arduino[3] < 1.25:
                 if led == 'left_led_on\n':
                     print("実験が正常に終了しました")
+                    mt_finish_time = datetime.now()
+                    mt_time = mt_finish_time - mt_start_time
                     break
                 else:
                     print("実験が異常終了しました。LEDは左側、選択したのは右側です")
@@ -138,6 +161,8 @@ if __name__ == '__main__':
             elif data_from_arduino[4] < 1.25:
                 if led == 'right_led_on\n':
                     print("実験が正常に終了しました")
+                    mt_finish_time = datetime.now()
+                    mt_time = mt_finish_time - mt_start_time
                     break
                 else:
                     print("実験が異常終了しました。LEDは右側、選択したのは左側です")
@@ -148,15 +173,32 @@ if __name__ == '__main__':
             print('ValueError')
             continue
 
+    # LEDを消す
+    all_led_off(ser)
+
     # フォルダを作成
-    if not os.path.exists(f'./data/{antilog}'):
-        os.makedirs(f'./data/{antilog}')
+    if not os.path.exists(f'./data/{id}'):
+        os.makedirs(f'./data/{id}')
 
     # 現在の時間を取得
     current_time = datetime.now().strftime('%Y%m%d_%H%M%S')
 
     # データをCSVファイルに保存
-    saved_df.to_csv(f'./data/{antilog}/{current_time}.csv', index=False)
+    saved_df.to_csv(f'./data/{id}/{current_time}.csv', index=False)
 
-    # LEDを消す
-    all_led_off(ser)
+    # 出力ファイル名
+    output_file = './data/summary.csv'
+
+    # もし、all.csvがなかったら作成する
+    if not os.path.exists(output_file):
+        all_df = pd.DataFrame(columns=['id', 'response_time', 'movement_time'])
+        all_df.to_csv(output_file, index=False)
+
+    # all.csvの末尾に追加
+    all_df = pd.read_csv(output_file)
+    all_df.loc[len(all_df)] = [id, rt_time.total_seconds(), mt_time.total_seconds()]
+    all_df.to_csv(output_file, index=False)
+
+    print(f"id: {id}")
+    print(f"rt_time: {rt_time}")
+    print(f"mt_time: {mt_time}")
